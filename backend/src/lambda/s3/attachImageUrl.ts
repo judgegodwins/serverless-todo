3d5fb51f5d4c4f23a5d30ba64112d1d7596ac4c4
judgegodwins <judgegodwins@gmail.com>
2022-11-26 18:05:09 +0100

    refactor: add datalayer and businesslogic

--- backend/src/lambda/s3/attachImageUrl.ts
+++ backend/src/lambda/s3/attachImageUrl.ts
@@ -1,6 +1,6 @@
 
 import { S3Handler, S3Event } from 'aws-lambda'
-import { updateTodoAttachment } from '../../helpers/todos';
+import { updateTodoAttachment } from '../../businessLogic/todos';
 
 export const handler: S3Handler = async (event: S3Event) => {
   await Promise.all(event.Records.map((record) => {
